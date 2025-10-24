8a382f23b393ed49236b7b9d22079ac8eb77bddb
M1GU1L1M <mssouza.miguel@gmail.com>
2025-10-24 11:05:22 -0300

    organizacao do projeto

---
+++ mavenproject/ce-sports-dao/src/main/java/org/ce/sports/dao/App.java
@@ -0,0 +1,13 @@
+package org.ce.sports.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
